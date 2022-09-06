ffadaa79a7d6db42dc219740c30fb748f30c4b94
channaveer <channaveersh38@gmail.com>
2022-09-06 14:05:45 +0530

    Changes

--- Calculator.java
+++ Calculator.java
@@ -14,3 +14,28 @@ public class Calculator {
 		return a * b;
 	}
 }
+
+We create a repo with main branch.
+Did some changes
+
+create feature-A branch
+create feature-B branch
+
+add few changes in feature-A
+add few changes in feature-B
+
+merge feature-A branch into main branch (Fats Forward)
+now main branch = main branch + feature-A branch
+
+switch to the feature-B branch -> $ git rebase main
+
+might be some conflict -> resolve conflict manually -> $ git add . -> $ git commit -m ""
+
+$git rebase --continue
+
+Now feature-B branch = main branch + feature-A branch + feature-B branch
+
+merged back from feature-B to main branch
+
+$ git checkout main
+$ git checkout feature-B (Fast Forward)
